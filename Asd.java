48479c73ff1c0b2b4a3ad742ce99b8f74d997c8e
Dmitrij <sdma7@mail.ru>
2023-02-25 14:10:08 +0300

    Asd

---
+++ Asd.java
@@ -0,0 +1,12 @@
+package OOP004seminar;
+
+public class Asd {
+    protected String name;
+    protected int weight;
+
+    public Asd(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+}
+
